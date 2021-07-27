51e54a43752757f73459431de84f54d69b9eba9c
Shimsuyeon <84979352+Shimsuyeon@users.noreply.github.com>
2021-07-27 23:23:32 +0900

    Create 10845_Queue.py

---
+++ BOJ/210727/10845_Queue.py
@@ -0,0 +1,48 @@
+import sys
+b = []
+def push(num):
+    a.append(num)
+def pop(a):
+    if a:
+        print(a[0])
+        a.pop(0)
+    elif not a: 
+        print(-1)
+def size(a):
+    print(len(a))
+def empty(a):
+    if not a:
+        print(1)
+    elif a:
+        print(0)
+def front(a):
+    if a:
+        print(a[0])
+    elif not a:
+        print(-1)
+def back(a):
+    if a:
+        print(a[-1])
+    elif not a:
+        print(-1)
+
+
+n =int(sys.stdin.readline().rstrip())
+a = []
+
+for _ in range(n):
+    input_split = sys.stdin.readline().rstrip().split()
+    order = input_split[0]
+
+    if order=='push' :
+        push(input_split[1])
+    elif order=='pop':
+        pop(a)
+    elif order=='size':
+        size(a)
+    elif order=='empty':
+        empty(a)
+    elif order=='front':
+        front(a)
+    elif order=='back':
+        back(a)
