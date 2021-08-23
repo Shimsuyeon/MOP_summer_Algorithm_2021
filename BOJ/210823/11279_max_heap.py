72297540d82a66b9331ff29c65299df7a839afdf
Shimsuyeon <84979352+Shimsuyeon@users.noreply.github.com>
2021-08-23 21:18:07 +0900

    Create 11279_max_heap.py

---
+++ BOJ/210823/11279_max_heap.py
@@ -0,0 +1,14 @@
+import sys
+import heapq
+
+n = int(input())
+heap = []
+for i in range(n):
+    x = int(sys.stdin.readline())
+    if x!=0:
+        heapq.heappush(heap,(-x,x))
+    else:
+        if len(heap) >=1:
+            print(heapq.heappop(heap)[1])
+        else:
+            print(0)
